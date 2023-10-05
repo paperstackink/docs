a07632f59d335840fe4fd85afc40b3c267b06ec1
Bjørn Lindholm <14328774+BjornDCode@users.noreply.github.com>
2023-10-05 12:53:12 +0900

    Render content page on mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,23 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./Pages/**/*.{md,mds,markdown,stencil}",
-    "./Components/**/*.{md,mds,markdown,stencil}",
+    // "./Pages/**/*.{md,mds,markdown,stencil}",
+    // "./Components/**/*.{md,mds,markdown,stencil}",
+    "./Output/**/*.html",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        accent: "#0000EE",
+      },
+      fontFamily: {
+        sans: ['"Favorit"', ...defaultTheme.fontFamily.sans],
+        headline: ['"Inter"', ...defaultTheme.fontFamily.sans],
+        mono: ['"MonoLisa"', ...defaultTheme.fontFamily.mono],
+      },
+    },
   },
   plugins: [],
 };
