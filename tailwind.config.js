2b780153f06678886b38d285368d970341c6a23c
Bjørn Lindholm <14328774+BjornDCode@users.noreply.github.com>
2023-10-05 09:16:22 +0900

    Add Tailwind locally

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./Pages/**/*.{md,mds,markdown,stencil}",
+    "./Components/**/*.{md,mds,markdown,stencil}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
